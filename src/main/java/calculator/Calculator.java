07b01dc1e20229ddf8d1dec355b011c1bf74edf0
Junho <jamlove4763@naver.com>
2025-10-19 09:24:33 +0000

    feat: 커스텀 문자열 분리 기능 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,9 +2,12 @@ package calculator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
     private String input;
+    public static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\r?\n+(.*)", Pattern.DOTALL);
 
     public Calculator(String input) {
         this.input = input;
@@ -17,13 +20,18 @@ public class Calculator {
 
     private List<Integer> split() {
         try {
-            //TODO: 추후 커스텀 구분자가 정규 표현식에 사용되는 문자일경우 '\\'이 구분자 앞에 붙어야 함.
-            String[] splitStr = this.input.split(",|:");
+            String[] splitInput;
+            if (this.input.startsWith("//")) {//커스텀 구분자를 사용하는 경우
+                Matcher matcher = CUSTOM_SEPARATOR_PATTERN.matcher(this.input);
+                if (!matcher.matches()) throw new IllegalArgumentException("잘못된 구분자가 입력되었습니다.");
+                splitInput = matcher.group(2).split(",|:|" + Pattern.quote(matcher.group(1)));
+            } else {
+                splitInput = this.input.split(",|:");
+            }
             List<Integer> result = new ArrayList<>();
-            for (String str: splitStr) {
-                Integer num = Integer.parseInt(str);
-                // 입력된 값이 음수일 경우 예외처리
-                if (num < 0) throw new NumberFormatException();
+            for (String str: splitInput) {
+                Integer num = Integer.parseInt(str.trim());
+                if (num < 0) throw new IllegalArgumentException("음수 값은 입력할 수 없습니다.");
                 result.add(num);
             }
             return result;
